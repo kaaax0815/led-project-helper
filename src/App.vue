23ad1dcc73a4df8b8aa0d18be75c1563e31d66d2
kaaax0815 <999999bst@gmail.com>
2023-05-12 00:19:20 +0200

    make footer responsive

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
+<script setup lang="ts">
+import AppFooter from '@/components/AppFooter.vue'
+</script>
+
 <template>
   <main class="flex-1">
     <router-view />
   </main>
-  <footer class="bg-quin">
-    <div class="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-400"
-        >© 2023 <a href="https://kaaaxcreators.de" class="hover:underline">Bernd Storath</a>. All
-        Rights Reserved.
-      </span>
-      <span class="text-sm text-gray-400">
-        Disclaimer: This site is not in any way affiliated with QuinLED or Intermittent Technology.
-      </span>
-    </div>
-  </footer>
+  <AppFooter />
 </template>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="bg-quin">
+    <div
+      class="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between p-4 text-center lg:flex-row"
+    >
+      <span class="text-sm text-gray-400"
+        >© 2023 <a href="https://kaaaxcreators.de" class="hover:underline">Bernd Storath</a>. All
+        Rights Reserved.
+      </span>
+      <span class="text-sm text-gray-400">
+        Disclaimer: This site is not in any way affiliated with QuinLED or Intermittent Technology.
+      </span>
+    </div>
+  </footer>
+</template>
