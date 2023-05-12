a34151ebaaea0de9f27f8f1d2ceb2e805511dbe5
kaaax0815 <999999bst@gmail.com>
2023-05-12 17:57:03 +0200

    Link source code in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,23 @@
+<script setup lang="ts">
+import FooterLink from './FooterLink.vue'
+</script>
+
 <template>
   <footer class="bg-quin">
     <div
       class="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between p-4 text-center lg:flex-row"
     >
       <span class="text-sm text-gray-400"
-        >© 2023 <a href="https://kaaaxcreators.de" class="hover:underline">Bernd Storath</a>. All
-        Rights Reserved.
+        >© 2023 <FooterLink link="https://kaaaxcreators.de">Bernd Storath</FooterLink>. All Rights
+        Reserved.
       </span>
       <span class="text-sm text-gray-400">
-        Disclaimer: This site is not in any way affiliated with QuinLED or Intermittent Technology.
+        <FooterLink link="https://github.com/kaaax0815/led-project-helper">Source Code</FooterLink>
+      </span>
+      <span class="text-sm text-gray-400">
+        Disclaimer: This site is not in any way affiliated with
+        <FooterLink link="https://quinled.info/">QuinLED</FooterLink>
+        or <FooterLink link="https://intermit.tech/">Intermittent Technology</FooterLink>.
       </span>
     </div>
   </footer>
---
+++ src/components/FooterLink.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+  link: string
+}>()
+</script>
+
+<template>
+  <a :href="link" target="_blank" class="hover:underline" rel="noopener noreferrer"><slot /></a>
+</template>
