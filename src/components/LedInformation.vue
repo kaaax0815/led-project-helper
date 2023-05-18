6d0bc7d28942b26cbee6473502be4f909874d6f5
kaaax0815 <999999bst@gmail.com>
2023-05-18 23:09:01 +0200

    more information

--- src/components/LedInformation.vue
+++ src/components/LedInformation.vue
@@ -8,26 +8,40 @@ const props = defineProps<{
   index: number
 }>()
 
-const strip = computed(() => {
+const information = computed(() => {
   const slot = strips.slots[props.index]
   if (!slot) {
     return null
   }
-  return slot.strip
+  const lEDs = slot.strip.lEDsPerMeter * slot.length
+  const pixels = lEDs / slot.strip.addressableBy
+  /** @Unit cm */
+  const pixelLength = (1 / slot.strip.lEDsPerMeter) * slot.strip.addressableBy * 100
+  return {
+    information: slot.strip.information,
+    buyLink: slot.strip.buyLink,
+    lEDs,
+    pixels,
+    pixelLength
+  }
 })
 </script>
 
 <template>
-  <div v-if="strip !== null">
+  <div v-if="information !== null">
     <p class="text-xl">Information</p>
     <ul>
-      <li v-for="info in strip.information" :key="info" class="ml-4 list-disc">
+      <li v-for="info in information.information" :key="info" class="ml-4 list-disc">
         {{ info }}
       </li>
     </ul>
-
+    <div>
+      <p>LEDs: {{ information.lEDs }}</p>
+      <p>Pixels: {{ information.pixels }}</p>
+      <p>Pixel Length: {{ information.pixelLength.toFixed(1) }}cm</p>
+    </div>
     <a
-      :href="strip.buyLink"
+      :href="information.buyLink"
       target="_blank"
       rel="noopener noreferrer"
       class="text-blue-400 underline"
